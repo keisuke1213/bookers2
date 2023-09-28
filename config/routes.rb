2c8cffed479df5f240f060afe4c8c1a76754cdd6
Keisuke1586 <fushanguifu@gmail.com>
2023-09-28 06:42:57 +0000

    [start] 課題４

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "homes#top"
+   get 'home/about' => 'homes#about' , as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]  do
+     resources :book_comments, only: [:create, :destroy]
+  end
    resources :users, only: [:index, :show, :edit, :update]
    
-   root to: "homes#top"
-   get 'home/about' => 'homes#about' , as: 'about'
-end
+   
+ end
 
 
